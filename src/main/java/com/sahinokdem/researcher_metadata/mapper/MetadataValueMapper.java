93540d25fcb5d903d063e5008b24ff7cbf0e8be5
Sahin Okdem <sahinokdem@gmail.com>
2024-07-23 16:38:24 +0300

    Added user with id exist check

--- src/main/java/com/sahinokdem/researcher_metadata/mapper/MetadataValueMapper.java
+++ src/main/java/com/sahinokdem/researcher_metadata/mapper/MetadataValueMapper.java
@@ -1,12 +1,22 @@
 package com.sahinokdem.researcher_metadata.mapper;
 
 import com.sahinokdem.researcher_metadata.entity.MetadataValue;
+import com.sahinokdem.researcher_metadata.entity.User;
+import com.sahinokdem.researcher_metadata.exception.BusinessExceptions;
 import com.sahinokdem.researcher_metadata.model.request.MetadataValueRequest;
 import com.sahinokdem.researcher_metadata.model.response.MetadataValueResponse;
+import com.sahinokdem.researcher_metadata.repository.UserRepository;
+import com.sahinokdem.researcher_metadata.service.UserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class MetadataValueMapper {
+
+    private final UserService userService;
+    private final UserRepository userRepository;
+
     public MetadataValueResponse toResponse(MetadataValue metadataValue) {
         return new MetadataValueResponse(
                 metadataValue.getId(),
@@ -17,10 +27,18 @@ public class MetadataValueMapper {
     }
 
     public MetadataValue toEntity(MetadataValueRequest request) {
+        assertUserExists(request.getUserId());
         return new MetadataValue(
                 request.getUserId(),
                 request.getRegistryId(),
                 request.getValue()
         );
+    }
+
+    private void assertUserExists(String userId) {
+        userRepository.findById(userId)
+                .orElseThrow(
+                        () -> BusinessExceptions.USER_NOT_FOUND
+                );
     }
 }
