28e7e2ae527dc0614c4089a583f8d95582acd96f
unknown <sahinokdem@gmail.com>
2024-08-04 02:39:42 +0300

    Added first get all values method, created test data

--- src/test/java/com/sahinokdem/researcher_metadata/controller/MetadataValueControllerTest.java
+++ src/test/java/com/sahinokdem/researcher_metadata/controller/MetadataValueControllerTest.java
@@ -1,14 +1,27 @@
 package com.sahinokdem.researcher_metadata.controller;
 
 import com.sahinokdem.researcher_metadata.TokenService;
+import com.sahinokdem.researcher_metadata.entity.User;
+import com.sahinokdem.researcher_metadata.model.response.MetadataRegistryResponse;
+import com.sahinokdem.researcher_metadata.model.response.MetadataValueResponse;
 import com.sahinokdem.researcher_metadata.repository.UserRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql("/test-data.sql")
 public class MetadataValueControllerTest {
 
     @Autowired
@@ -18,5 +31,29 @@ public class MetadataValueControllerTest {
     @Autowired
     private UserRepository userRepository;
 
-
+    @Test
+    public void given_EditorUser_when_GetAllValues_then_StatusOk_and_AllValuesReturned() {
+        //GIVEN
+        User user = userRepository.findByEmail("editor@test.com").orElseThrow(
+                () -> new RuntimeException("Invalid Test Data Provided")
+        );
+        String editorToken = tokenService.getTokenFor(user);
+        //WHEN
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + editorToken);
+        HttpEntity<Void> request = new HttpEntity<>(headers);
+        ResponseEntity<List<MetadataValueResponse>> response = restTemplate.exchange(
+                "/metadata-value", HttpMethod.GET, request, new ParameterizedTypeReference<List<MetadataValueResponse>>() {}
+        );
+        //THEN
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        List<MetadataValueResponse> metadataRegistries = response.getBody();
+        assertAll(
+                () -> assertFalse(metadataRegistries.isEmpty(), "Value list should not be empty"),
+                () -> assertTrue(metadataRegistries.stream().allMatch(eachValue ->
+                        eachValue.getId() != null && eachValue.getUserId() != null &&
+                                eachValue.getRegistryId() != null && eachValue.getValue() != null
+                ), "All values should have IDs, names and types")
+        );
+    }
 }
---
+++ src/test/resources/test-data.sql
@@ -0,0 +1,6 @@
+INSERT INTO metadata_value (id, created_date, modified_date, owner_id, metadata_registry_id, value)
+VALUES ('65358564-927d-447c-a832-c5c263ada7bd', now(), now(), 'c9a2f3d2-7b8b-4b32-9101-dc223b6c5b8a', 'c9a2f3d2-7b8b-4b32-9101-dc223b6c5b4a', 'value');
+
+INSERT INTO metadata_value (id, created_date, modified_date, owner_id, metadata_registry_id, value)
+VALUES ('45358564-927d-447c-a832-c5c263ada7bc', now(), now(), 'c9a2f3d2-7b8b-4b32-9101-dc223b6c5b8b', 'e8f2a71d-4b9c-4a7e-8c3f-5d1a8f9b2c6e', 2);
+
