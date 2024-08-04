40d66a31cb7475d37b3a3e2878eb44c4f401e3a3
unknown <sahinokdem@gmail.com>
2024-08-04 03:07:23 +0300

    Added get all values methods

--- src/test/java/com/sahinokdem/researcher_metadata/controller/MetadataRegistryControllerTest.java
+++ src/test/java/com/sahinokdem/researcher_metadata/controller/MetadataRegistryControllerTest.java
@@ -24,6 +24,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class MetadataRegistryControllerTest {
 
     @Autowired
--- src/test/java/com/sahinokdem/researcher_metadata/controller/MetadataValueControllerTest.java
+++ src/test/java/com/sahinokdem/researcher_metadata/controller/MetadataValueControllerTest.java
@@ -2,9 +2,12 @@ package com.sahinokdem.researcher_metadata.controller;
 
 import com.sahinokdem.researcher_metadata.TokenService;
 import com.sahinokdem.researcher_metadata.entity.User;
+import com.sahinokdem.researcher_metadata.exception.BusinessExceptions;
+import com.sahinokdem.researcher_metadata.exception.ErrorDto;
 import com.sahinokdem.researcher_metadata.model.response.MetadataRegistryResponse;
 import com.sahinokdem.researcher_metadata.model.response.MetadataValueResponse;
 import com.sahinokdem.researcher_metadata.repository.UserRepository;
+import com.sahinokdem.researcher_metadata.util.TestUtils;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +15,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.jdbc.Sql;
 
 import java.util.List;
@@ -22,6 +26,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Sql("/test-data.sql")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class MetadataValueControllerTest {
 
     @Autowired
@@ -47,13 +52,76 @@ public class MetadataValueControllerTest {
         );
         //THEN
         assertEquals(HttpStatus.OK, response.getStatusCode());
-        List<MetadataValueResponse> metadataRegistries = response.getBody();
+        List<MetadataValueResponse> metadataValues = response.getBody();
         assertAll(
-                () -> assertFalse(metadataRegistries.isEmpty(), "Value list should not be empty"),
-                () -> assertTrue(metadataRegistries.stream().allMatch(eachValue ->
+                () -> assertFalse(metadataValues.isEmpty(), "Value list should not be empty"),
+                () -> assertTrue(metadataValues.stream().allMatch(eachValue ->
                         eachValue.getId() != null && eachValue.getUserId() != null &&
                                 eachValue.getRegistryId() != null && eachValue.getValue() != null
                 ), "All values should have IDs, names and types")
         );
+    }
+
+    @Test
+    public void given_ResearcherUser_when_GetAllValues_then_StatusOk_and_AllValuesWhichResearcherHasReturned() {
+        //GIVEN
+        User user = userRepository.findByEmail("researcher@test.com").orElseThrow(
+                () -> new RuntimeException("Invalid Test Data Provided")
+        );
+        String researcherToken = tokenService.getTokenFor(user);
+        //WHEN
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + researcherToken);
+        HttpEntity<Void> request = new HttpEntity<>(headers);
+        ResponseEntity<List<MetadataValueResponse>> response = restTemplate.exchange(
+                "/metadata-value", HttpMethod.GET, request, new ParameterizedTypeReference<List<MetadataValueResponse>>() {}
+        );
+        //THEN
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        List<MetadataValueResponse> metadataValues = response.getBody();
+        assertAll(
+                () -> assertFalse(metadataValues.isEmpty(), "Value list should not be empty"),
+                () -> assertTrue(metadataValues.stream().allMatch(eachValue ->
+                        eachValue.getId() != null && eachValue.getUserId() != null &&
+                                eachValue.getRegistryId() != null && eachValue.getValue() != null  &&
+                                eachValue.getUserId().equals(user.getId())
+                ), "All values should have IDs, names and types")
+        );
+    }
+
+    @Test
+    public void given_AdminUser_when_GetAllValues_then_Forbidden() {
+        //GIVEN
+        User user = userRepository.findByEmail("admin@test.com").orElseThrow(
+                () -> new RuntimeException("Invalid Test Data Provided")
+        );
+        String adminToken = tokenService.getTokenFor(user);
+        //WHEN
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + adminToken);
+        HttpEntity<Void> request = new HttpEntity<>(headers);
+        ResponseEntity<ErrorDto> response = restTemplate.exchange(
+                "/metadata-value", HttpMethod.GET, request, new ParameterizedTypeReference<ErrorDto>() {}
+        );
+        //THEN
+        TestUtils.assertErrorDto(BusinessExceptions.AUTHORIZATION_MISSING, response);
+    }
+
+    @Test
+    public void given_HRSpecialistUser_when_GetAllValues_then_Forbidden() {
+        //GIVEN
+        User user = userRepository.findByEmail("hr_specialist@test.com").orElseThrow(
+                () -> new RuntimeException("Invalid Test Data Provided")
+        );
+        String hrSpecialistToken = tokenService.getTokenFor(user);
+        //WHEN
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + hrSpecialistToken);
+        HttpEntity<Void> request = new HttpEntity<>(headers);
+        ResponseEntity<ErrorDto> response = restTemplate.exchange(
+                "/metadata-value", HttpMethod.GET, request, new ParameterizedTypeReference<ErrorDto>() {}
+        );
+        //THEN
+        TestUtils.assertErrorDto(BusinessExceptions.AUTHORIZATION_MISSING, response);
     }
 }
