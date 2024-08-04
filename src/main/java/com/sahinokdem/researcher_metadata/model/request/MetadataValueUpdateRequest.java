ede331ccb833e33c54045e92a953df00cbcd949e
unknown <sahinokdem@gmail.com>
2024-08-04 15:05:18 +0300

    Fixed update problem

--- src/main/java/com/sahinokdem/researcher_metadata/model/request/MetadataValueUpdateRequest.java
+++ src/main/java/com/sahinokdem/researcher_metadata/model/request/MetadataValueUpdateRequest.java
@@ -1,10 +1,17 @@
 package com.sahinokdem.researcher_metadata.model.request;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import javax.validation.constraints.NotEmpty;
 
 @Data
 public class MetadataValueUpdateRequest {
-    @NotEmpty
+    @NotEmpty(message = "")
     private final String value;
+
+    @JsonCreator
+    public MetadataValueUpdateRequest(@JsonProperty String  value) {
+        this.value = value;
+    }
 }
