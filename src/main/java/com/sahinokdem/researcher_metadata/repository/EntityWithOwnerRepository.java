5517eb75767cb0a5dcfed5d19c3893250d50d4ef
unknown <sahinokdem@gmail.com>
2024-08-08 02:18:41 +0300

    Used EntityAccessService in MetadataValueService

--- src/main/java/com/sahinokdem/researcher_metadata/repository/EntityWithOwnerRepository.java
+++ src/main/java/com/sahinokdem/researcher_metadata/repository/EntityWithOwnerRepository.java
@@ -4,9 +4,12 @@ import com.sahinokdem.researcher_metadata.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 import java.util.Optional;
 
 @NoRepositoryBean
 public interface EntityWithOwnerRepository<T> extends JpaRepository<T, String> {
      Optional<T> findByOwnerAndId(User owner, String id);
+     List<T> findAllByOwner(User owner);
 }
--- src/main/java/com/sahinokdem/researcher_metadata/service/EntityAccessService.java
+++ src/main/java/com/sahinokdem/researcher_metadata/service/EntityAccessService.java
@@ -7,11 +7,8 @@ import com.sahinokdem.researcher_metadata.exception.BusinessException;
 import com.sahinokdem.researcher_metadata.exception.BusinessExceptions;
 import com.sahinokdem.researcher_metadata.repository.EntityWithOwnerRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.data.jpa.domain.Specification;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
-
-import java.util.function.Supplier;
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -30,6 +27,18 @@ public class EntityAccessService {
         } else if (userRoleService.checkCurrentUserRole(accessUserRole)) {
             return entityRepository.findById(entityId)
                     .orElseThrow(() -> entityNotFoundException);
+        } else {
+            throw BusinessExceptions.AUTHORIZATION_MISSING;
+        }
+    }
+
+    public <T extends BaseEntity> List<T> getAllEntities(EntityWithOwnerRepository<T> entityRepository,
+                 UserRole ownerUserRole, UserRole accessUserRole) {
+        if (userRoleService.checkCurrentUserRole(ownerUserRole)) {
+            User owner = authenticationService.getAuthenticatedUser();
+            return entityRepository.findAllByOwner(owner);
+        } else if (userRoleService.checkCurrentUserRole(accessUserRole)) {
+            return entityRepository.findAll();
         } else {
             throw BusinessExceptions.AUTHORIZATION_MISSING;
         }
