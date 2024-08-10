a438482db8d2f3e5186cfddf09ce9b263358d45d
unknown <sahinokdem@gmail.com>
2024-08-10 07:32:20 +0300

    Removed unnecessary state

--- src/main/java/com/sahinokdem/researcher_metadata/enums/State.java
+++ src/main/java/com/sahinokdem/researcher_metadata/enums/State.java
@@ -3,7 +3,4 @@ package com.sahinokdem.researcher_metadata.enums;
 public enum State {
     Q0, // Ready to send form
     Q1, // Sent form, Ready to send CV
-    Q2, // Rejected first cv, Ready to send second CV
-
-    State()
 }
